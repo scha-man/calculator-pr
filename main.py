8beeaf9d046bcdd832784417fc17e59f9a27c679
oliver <oliver_weil@hotmail.com>
2025-11-11 20:09:32 +0100

    Adding substraction, mate

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def calculate(a, op, b):
     if op == '+':
         return a + b
+    elif op == '-':
+        return a - b
     return None
 
 def main():
     assert calculate(3, '+', 2) == 5
+    assert calculate(4, '-', 2 ) == 2
 
 if __name__ == '__main__':
     main()
